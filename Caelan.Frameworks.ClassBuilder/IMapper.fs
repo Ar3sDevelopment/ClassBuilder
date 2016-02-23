48a7ff19a72c825e3f70622b79422b48f949325b
Matteo Bruni <ar3s@icloud.com>
2016-02-23 09:46:51 +0100

    startup

---
+++ Caelan.Frameworks.ClassBuilder/IMapper.fs
@@ -0,0 +1,18 @@
+﻿namespace Caelan.Frameworks.ClassBuilder.Interfaces
+
+type IMapper =
+    interface
+    end
+
+type IMapper<'TSource, 'TDestination> = 
+    inherit IMapper
+    /// <summary>
+    /// 
+    /// </summary>
+    abstract Map : 'TSource -> 'TDestination
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="source"></param>
+    /// <param name="destination"></param>
+    abstract Map : source: 'TSource * destination:'TDestination -> 'TDestination
---
+++ Caelan.Frameworks.ClassBuilder/MapFieldAttribute.fs
@@ -0,0 +1,8 @@
+﻿namespace Caelan.Frameworks.ClassBuilder.Attributes
+open System
+
+[<AllowNullLiteral>]
+[<AttributeUsage(AttributeTargets.Field ||| AttributeTargets.Property)>]
+type MapFieldAttribute(toField : string) = 
+    inherit Attribute()
+    member __.ToField = toField
