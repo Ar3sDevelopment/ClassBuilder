39c799597ecdcdc9bb285c486d3710f1cebae771
Matteo Bruni <ar3s@icloud.com>
2016-02-26 11:00:09 +0100

    missing files

---
+++ Caelan.Frameworks.ClassBuilder.NUnit.Common/Mappers.fs
@@ -0,0 +1,18 @@
+﻿namespace Caelan.Frameworks.ClassBuilder.NUnit.Common
+
+open Caelan.Frameworks.ClassBuilder.Classes
+open Classes
+
+module Mappers = 
+    type ABMapper() = 
+        inherit DefaultMapper<TestA, TestB>()
+        override __.CustomMap(source, dest) = 
+            base.CustomMap(source, dest) |> ignore
+            dest.B <- dest.B + " mapper"
+            dest
+    
+    type IntFloatMapper() = 
+        inherit DefaultMapper<int, float>()
+        override __.CustomMap(source, dest) = 
+            base.CustomMap(source, dest) |> ignore
+            (float) source
